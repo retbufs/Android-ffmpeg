0e9b854235402e65c7d658aefbe2d30961b50208
zhenio <zhenio@126.com>
2020-01-08 11:24:36 +0800

    first commit

---
+++ libavfilter/af_resample.d
@@ -0,0 +1,20 @@
+libavfilter/af_resample.o: libavfilter/af_resample.c libavutil/avassert.h \
+ libavutil/avutil.h libavutil/common.h libavutil/attributes.h \
+ libavutil/macros.h libavutil/version.h libavutil/avconfig.h config.h \
+ libavutil/intmath.h libavutil/mem.h libavutil/error.h \
+ libavutil/internal.h libavutil/timer.h libavutil/log.h libavutil/cpu.h \
+ libavutil/dict.h libavutil/pixfmt.h libavutil/libm.h \
+ libavutil/intfloat.h libavutil/mathematics.h libavutil/rational.h \
+ libavutil/avstring.h libavutil/common.h libavutil/dict.h \
+ libavutil/mathematics.h libavutil/opt.h libavutil/samplefmt.h \
+ libavresample/avresample.h libavutil/avutil.h libavutil/channel_layout.h \
+ libavutil/frame.h libavutil/buffer.h libavutil/log.h \
+ libavresample/version.h libavutil/version.h libavfilter/audio.h \
+ libavfilter/avfilter.h libavutil/attributes.h libavutil/buffer.h \
+ libavutil/samplefmt.h libavutil/pixfmt.h libavutil/rational.h \
+ libavfilter/version.h libavfilter/internal.h libavutil/internal.h \
+ libavfilter/formats.h libavfilter/framepool.h libavfilter/framequeue.h \
+ libavfilter/thread.h libavfilter/version.h libavfilter/video.h \
+ libavcodec/avcodec.h libavutil/cpu.h libavutil/hwcontext.h \
+ libavutil/frame.h libavcodec/version.h libavcodec/internal.h \
+ libavcodec/avcodec.h
---
+++ libavformat/avc.d
@@ -0,0 +1,17 @@
+libavformat/avc.o: libavformat/avc.c libavutil/intreadwrite.h \
+ libavutil/avconfig.h libavutil/attributes.h libavutil/bswap.h config.h \
+ libavcodec/h264.h libavcodec/get_bits.h libavutil/common.h \
+ libavutil/macros.h libavutil/version.h libavutil/intmath.h \
+ libavutil/common.h libavutil/mem.h libavutil/error.h libavutil/avutil.h \
+ libavutil/rational.h libavutil/mathematics.h libavutil/intfloat.h \
+ libavutil/log.h libavutil/pixfmt.h libavutil/internal.h \
+ libavutil/timer.h libavutil/cpu.h libavutil/dict.h libavutil/libm.h \
+ libavutil/log.h libavutil/avassert.h libavcodec/avcodec.h \
+ libavutil/samplefmt.h libavutil/attributes.h libavutil/avutil.h \
+ libavutil/buffer.h libavutil/cpu.h libavutil/channel_layout.h \
+ libavutil/dict.h libavutil/frame.h libavutil/buffer.h \
+ libavutil/samplefmt.h libavutil/hwcontext.h libavutil/frame.h \
+ libavutil/pixfmt.h libavutil/rational.h libavcodec/version.h \
+ libavutil/version.h libavcodec/mathops.h libavutil/reverse.h \
+ libavcodec/vlc.h libavformat/avformat.h libavcodec/avcodec.h \
+ libavformat/avio.h libavformat/version.h libavformat/avc.h
